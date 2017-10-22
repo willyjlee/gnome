4d25b097f2587d0ad70c63c2a534acb705102666
willyjlee <williamjlee@ucla.edu>
2017-10-22 11:15:00 -0700

    clean

--- nn.py
+++ nn.py
@@ -8,31 +8,31 @@ knc = KNeighborsClassifier(n_neighbors=num_neighbors, algorithm='kd_tree')
 
 with open('data/pheno.features', 'r') as f:
     features = [str.rstrip() for str in f.readlines()]
-    print(features)
+    #print(features)
 # - 1 for the username in col0
 num_features = len(features) - 1
 
 with open('data/phenotypes - scores_pseudo_users.csv', 'r') as line:
     all_features = [x.rstrip() for x in line.readline().split(',')]
-    print(all_features)
+    #print(all_features)
 
 selected_inds = []
 for feature in features:
     selected_inds.append(all_features.index(feature))
-print(selected_inds)
+#print(selected_inds)
 
 def get_nearest(phenos, file=None, users=None):
-    print('phenos')
-    print(phenos.shape)
+    #print('phenos')
+    #print(phenos.shape)
     make_phenos = np.array([phenos[selected_inds[0]]])
     for i in range(len(selected_inds)):
         if i != 0:
             make_phenos = np.concatenate((make_phenos, np.array([phenos[selected_inds[i]]])), axis=0)
-    print('make')
-    print(make_phenos.shape)
+    #print('make')
+    #print(make_phenos.shape)
     phenos = make_phenos[1:]
-    print('phenos')
-    print(phenos.shape)
+    #print('phenos')
+    #print(phenos.shape)
 
     if file is not None:
         pheno = pd.read_csv(file, usecols=features)
@@ -40,18 +40,18 @@ def get_nearest(phenos, file=None, users=None):
 
     elif users is not None:
         # select features, assume same order
-        print(selected_inds[0])
-        print('users')
-        print(users.shape)
+        #print(selected_inds[0])
+        #print('users')
+        #print(users.shape)
         pheno = np.array([users[:, selected_inds[0]]])
-        print(pheno.shape)
+        #print(pheno.shape)
         for i in range(len(selected_inds)):
             if i != 0:
                 pheno = np.concatenate((pheno, np.array([users[:, selected_inds[i]]])), axis=0)
         pheno = np.transpose(pheno)
 
-        print('pheno')
-        print(pheno)
+        #print('pheno')
+        #print(pheno)
         full = pheno
     else:
         return
@@ -59,23 +59,23 @@ def get_nearest(phenos, file=None, users=None):
     # username in first
     data = full[:, 1:]
 
-    print('full')
-    print(full.shape)
+    #print('full')
+    #print(full.shape)
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
             if not isinstance(data[i][j], str) and math.isnan(data[i][j]):
                 data[i][j] = 0
 
     knc.fit(data, range(data.shape[0]))
-    print('data')
-    print(data.shape)
+    #print('data')
+    #print(data.shape)
 
     dist, inds = knc.kneighbors([phenos])
-    print(inds)
+    #print(inds)
     #inds = inds[:, 1:]
     ppl = full[inds]
     print("Found {} nearest neighbors, using {} features:".format(num_neighbors, num_features))
-    print(ppl)
+    #print(ppl)
     return ppl
 
 if __name__ == '__main__':
@@ -83,4 +83,17 @@ if __name__ == '__main__':
     #get_nearest(range(21), file='data/phenotypes - scores_pseudo_users.csv')
 
     #get_nearest(np.array(inp), users=np.array([[i+2 for i in range(66)], [i for i in range(66)], [i+1 for i in range(66)]]))
-    get_nearest(np.array([i % 4 for i in range(66)]), users=np.random.randint(0, high=4, size=(2, 66)))
+    #arrays = np.random.randint(0, high=4, size=(2,65))
+    #print(arrays.shape)
+    arrays = np.array([["user0"],["user1"], ["user2"], ["user3"], ["user4"]])
+    arrays = np.concatenate((arrays, np.random.randint(0, high=4, size=(5,65))), axis=1)
+    #print(arrays)
+    # arrays.itemset((0,0), "user0")
+    # arrays.itemset((1,0), "user1")
+    inp = ["input_user"]
+    for i in range(65):
+        inp.append(i%4)
+    print('input')
+    print(inp)
+    nns = get_nearest(np.array(inp), users=arrays)
+    print(nns)
