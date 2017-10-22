ee89e454e5af08d98e3a5ac27c8b2ea08fcde5e9
willyjlee <williamjlee@ucla.edu>
2017-10-22 10:42:07 -0700

    response

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, json
+import numpy as np
 import nn
 
 app = Flask(__name__)
@@ -8,7 +9,21 @@ def get():
     data = request.get_json()
     print(data)
     # nns = nn.get(phenos)
+    nns = nn.get_nearest(np.array(data['input']),
+                users=np.array([data['user0'], data['user1'], data['user2'], data['user3'], data['user4'], data['user5']]))
+    nns = np.squeeze(nns, axis=0)
+
+    resp = {}
+    neighbors = []
+    for i in range(nns.shape[0]):
+        neighbors.append(nns[i][0])
+    resp['resp'] = neighbors
+
+    return json.dumps(resp)
 
 
 if __name__ == '__main__':
+    # nns = nn.get_nearest(np.array(range(66)), users=np.array([[i+1 for i in range(66)], [i+2 for i in range(66)]]))
+    # print(nns.shape)
+
     app.run(debug=True)
--- nn.py
+++ nn.py
@@ -6,34 +6,82 @@ import math
 num_neighbors = 2
 knc = KNeighborsClassifier(n_neighbors=num_neighbors, algorithm='kd_tree')
 
-def get(phenos):
-    with open('data/pheno.features', 'r') as f:
-        features = [str.rstrip() for str in f.readlines()]
-        print(features)
-    pheno = pd.read_csv('data/phenotypes - scores_pseudo_users.csv', usecols=features)
+with open('data/pheno.features', 'r') as f:
+    features = [str.rstrip() for str in f.readlines()]
+    print(features)
+# - 1 for the username in col0
+num_features = len(features) - 1
 
-    # features = ['name', 'id', 'anger', 'depression']
-    # pheno = pd.read_csv('data/pheno.csv', names=features)
-    num_features = len(features) - 1
-    #print("num_features: %s" % (num_features))
+with open('data/phenotypes - scores_pseudo_users.csv', 'r') as line:
+    all_features = [x.rstrip() for x in line.readline().split(',')]
+    print(all_features)
 
-    full = np.nan_to_num(pheno.as_matrix())
-    for i in range(full.shape[0]):
-        for j in range(full.shape[1]):
-            if not isinstance(full[i][j], str) and math.isnan(full[i][j]):
-                full[i][j] = 0
+selected_inds = []
+for feature in features:
+    selected_inds.append(all_features.index(feature))
+print(selected_inds)
 
-    data = full[:, (len(features) - num_features):]
-    print(data.shape)
+def get_nearest(phenos, file=None, users=None):
+    print('phenos')
+    print(phenos.shape)
+    make_phenos = np.array([phenos[selected_inds[0]]])
+    for i in range(len(selected_inds)):
+        if i != 0:
+            make_phenos = np.concatenate((make_phenos, np.array([phenos[selected_inds[i]]])), axis=0)
+    print('make')
+    print(make_phenos.shape)
+    phenos = make_phenos[1:]
+    print('phenos')
+    print(phenos.shape)
+
+    if file is not None:
+        pheno = pd.read_csv(file, usecols=features)
+        full = np.nan_to_num(pheno.as_matrix())
+
+    elif users is not None:
+        # select features, assume same order
+        print(selected_inds[0])
+        print('users')
+        print(users.shape)
+        pheno = np.array([users[:, selected_inds[0]]])
+        print(pheno.shape)
+        for i in range(len(selected_inds)):
+            if i != 0:
+                pheno = np.concatenate((pheno, np.array([users[:, selected_inds[i]]])), axis=0)
+        pheno = np.transpose(pheno)
+
+        print('pheno')
+        print(pheno.shape)
+        full = pheno
+    else:
+        return
+
+    # username in first
+    data = full[:, 1:]
+
+    print('full')
+    print(full.shape)
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            if not isinstance(data[i][j], str) and math.isnan(data[i][j]):
+                data[i][j] = 0
 
     knc.fit(data, range(data.shape[0]))
+    print('data')
+    print(data.shape)
 
     dist, inds = knc.kneighbors([phenos])
     #inds = inds[:, 1:]
     ppl = full[inds]
     print("Found {} nearest neighbors, using {} features:".format(num_neighbors, num_features))
     print(ppl)
+    return ppl
 
 if __name__ == '__main__':
     # examples
-    get(range(21))
+    #get_nearest(range(21), file='data/phenotypes - scores_pseudo_users.csv')
+    inp = list(range(66))
+
+    print(inp)
+    get_nearest(np.array(inp), users=np.array([[i for i in range(66)], [i for i in range(66)]]))
+
